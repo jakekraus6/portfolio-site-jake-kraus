fff3fc3d3a42be852dae4b72784538fe09f261d0
jakekraus6 <xjakekrausx16@gmail.com>
2024-11-20 01:26:56 -0800

    jQuery updates
    Added jQuery updates.

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,17 @@
 console.log("hello world")
 console.log("Welcome to my webpage!")
+function menuToggle() {
+    var x= document.getElementById ('myNavtoggle');
+}
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    x.className += ' responsive';
+  }
+  function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
